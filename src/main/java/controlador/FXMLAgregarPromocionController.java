073eede25876d90b0d6de0a53063820acb98351a
irvin <irving_cena2@hotmail.com>
2019-05-31 08:38:41 -0500

    Se agregó las interfaces y controllers de administrarPromociones y RegistrarPromocion

---
+++ src/main/java/controlador/FXMLAgregarPromocionController.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import persistencia.Conexion;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javax.swing.JOptionPane;
+import modelo.Promocion;
+import com.uv.gimnasio.MainApp;
+import persistencia.PromocionDatos;
+/**
+ * FXML Controller class
+ *
+ * @author irvin
+ */
+public class FXMLAgregarPromocionController implements Initializable {
+    MainApp programaPrincipal;
+    Conexion controlador = new Conexion();
+    PromocionDatos promo = new PromocionDatos();
+    @FXML private JFXTextField tfRnombre;
+    @FXML private JFXDatePicker dpRfechaInicio;
+    @FXML private JFXDatePicker dpRfechaFin;
+    @FXML private JFXTextArea taRdescripcion;
+    @FXML private JFXTextField tfRdescuento;
+    @FXML private JFXTextField tfRid;
+    @FXML ObservableList<Promocion> promocionest;
+    @FXML private JFXButton btnRegistrar;
+    @FXML private JFXButton btnCancelar;
+    @FXML private JFXButton btnRegresar;
+    
+  public void setProgramaPrincipal(MainApp programaPrincipal){
+    this.programaPrincipal = programaPrincipal;
+  }
+  @FXML
+  public void registrarPromocion(ActionEvent event){
+      String nombre = tfRnombre.getText();
+      int montoDescuento = Integer.parseInt(tfRdescuento.getText());
+      LocalDate fecha_inicio = dpRfechaInicio.getValue();
+      LocalDate fecha_fin = dpRfechaFin.getValue();
+      String descripcion = taRdescripcion.getText();
+      int idPromocion = Integer.parseInt(tfRid.getText());
+      
+      if((nombre.equals("") != true) && ("".equals(montoDescuento) != true) && (fecha_inicio.equals("") != true) && (fecha_fin.equals("") != true) && (descripcion.equals("") != true) && ("".equals(idPromocion) != true)){
+          Promocion promocion = new Promocion(idPromocion,nombre,fecha_inicio,fecha_fin,montoDescuento,descripcion);
+          promo.nuevaPromocion(promocion);
+          promocionest = FXCollections.observableArrayList();
+          promocionest.add(promocion);
+          JOptionPane.showMessageDialog(null, "Promocion agregada exitosamente");
+          limpiar();
+      }else{
+        JOptionPane.showMessageDialog(null, "Rellene todos los campos");
+      }
+  }
+  @FXML
+  public void cancelarRegistro(ActionEvent event){
+    limpiar();
+  }
+    @FXML
+    private void limpiar() {
+        tfRnombre.setText("");
+        tfRdescuento.setText("");
+        dpRfechaInicio.getEditor().clear();
+        dpRfechaFin.getEditor().clear();
+        taRdescripcion.setText("");
+        tfRid.setText("");
+    }
+ /*  @FXML
+   public void regresarAdministracion(ActionEvent event){
+      try {
+            this.programaPrincipal.mostrarVentanaAdministrarPromocion();
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLAgregarPromocionController.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+   }*/
+   public void colocarImagenBoton(){
+      URL linkFlecha = getClass().getResource("/images/flecha.png");
+      Image imagenFlecha = new Image(linkFlecha.toString(),100,60,false,true);
+      
+      btnRegresar.setGraphic((new ImageView (imagenFlecha)));
+      
+    }
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+    colocarImagenBoton();
+  }  
+  
+}
---
+++ src/main/java/controlador/FXMLController.java
@@ -0,0 +1,246 @@
+package controlador;
+
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import com.uv.gimnasio.MainApp;
+import persistencia.Conexion;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import javax.swing.JOptionPane;
+import modelo.Promocion;
+import persistencia.PromocionDatos;
+
+public class FXMLController implements Initializable {
+    MainApp programaPrincipal;
+    @FXML ObservableList<Promocion> promocionest;
+    @FXML private Label label;
+    @FXML private TableView<Promocion> tablaPromociones;
+    @FXML private TableColumn<Promocion, String> tcNombre;
+    @FXML private TableColumn<Promocion, String> tcDescuento;
+    @FXML private TableColumn<Promocion, String> tcFechaInicio;
+    @FXML private TableColumn<Promocion, String> tcFechaFin;
+    @FXML private TableColumn<Promocion, String> tcDescripcion;
+    @FXML private TableColumn<Promocion, String> tcIdPromocion;
+    
+    @FXML private JFXButton btnModificar;
+    @FXML private JFXButton btnGuardar;
+    @FXML private JFXButton btnEliminar;
+    @FXML private JFXButton btnCancelar;
+    @FXML private JFXButton btnRegistrar;
+    
+    @FXML private JFXTextField tfnombre;
+    @FXML private JFXTextField tfDescuento;
+    @FXML private JFXTextField tfID;
+    
+    @FXML private JFXDatePicker dpFechaInicio;
+    @FXML private JFXDatePicker dpFechaFin;
+    
+    @FXML private JFXTextArea taDescripcion;
+    PromocionDatos promo = new PromocionDatos();
+    //Conexion controlador = new Conexion();
+    /**
+     * Esta funcion sirve para mostra el contenido en la tabla
+     */
+    public void cargarTablaPromociones(){
+      btnGuardar.setVisible(false);
+      btnCancelar.setVisible(false);
+      try{
+        tcNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        tcDescuento.setCellValueFactory(new PropertyValueFactory("montoDescuento"));
+        tcFechaInicio.setCellValueFactory(new PropertyValueFactory("fechaInicio"));
+        tcFechaFin.setCellValueFactory(new PropertyValueFactory("fechaFin"));
+        tcDescripcion.setCellValueFactory(new PropertyValueFactory("descripcion"));
+       // tcIdPromocion.setCellValueFactory(new PropertyValueFactory("id_promocion"));
+        tcIdPromocion.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Promocion, String>, ObservableValue<String>>() {
+      public ObservableValue<String> call(TableColumn.CellDataFeatures<Promocion, String> p) {
+        String idPromocion = Integer.toString(p.getValue().getId());
+        return new SimpleStringProperty(idPromocion);
+      }
+    });
+        List<Promocion> promocionesD = promo.getPromociones();
+        promocionest = FXCollections.observableArrayList();
+        
+        for(Promocion promocion : promocionesD){
+          promocionest.add(promocion);
+        }
+       tablaPromociones.setItems(promocionest);
+      }catch (SQLException ex){
+        Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+    
+    @FXML
+    private Promocion obtenerPromocionSeleccionada(){
+      if(tablaPromociones != null){
+        Promocion promocion = tablaPromociones.getSelectionModel().getSelectedItem();
+        return promocion;
+      }else{
+        return null;
+      }
+    }
+    
+    @FXML
+    private void modificarPromocion(ActionEvent event){
+       
+       int posicion = tablaPromociones.getSelectionModel().getSelectedIndex();
+       if(posicion != -1){
+         btnEliminar.setVisible(false);
+         btnModificar.setVisible(false);
+         btnGuardar.setVisible(true);
+        btnCancelar.setVisible(true);
+         Promocion promocion = obtenerPromocionSeleccionada();
+         tablaPromociones.setDisable(true);
+         tfnombre.setText(promocion.getNombre());
+         tfDescuento.setText(Double.toString(promocion.getMontoDescuento()));
+         dpFechaInicio.setValue(promocion.getFecha_inicio());
+         dpFechaFin.setValue(promocion.getFecha_fin());
+         taDescripcion.setText(promocion.getDescripcion());
+         tfID.setText(Integer.toString(promocion.getId()));
+         
+         
+       }else{
+         JOptionPane.showMessageDialog(null, "Selecciona una promocion");
+       }
+    }
+    
+    @FXML
+    private void guardarCambiosPromocion(ActionEvent event) {
+        tablaPromociones.setDisable(false);
+        String nombre = tfnombre.getText();
+        double montoDescuento = Integer.parseInt(tfDescuento.getText());
+        LocalDate fechaInicio = dpFechaInicio.getValue();
+        LocalDate fechaFin = dpFechaFin.getValue();
+        String descripcion = taDescripcion.getText();
+        int idPromocion = (Integer.parseInt(tfID.getText()));
+        Promocion promocion = new Promocion(idPromocion,nombre,fechaInicio,fechaFin,montoDescuento, descripcion);
+   
+        if (("".equals(idPromocion) != true) && (nombre.equals("") != true)
+                && ("".equals(montoDescuento) != true) && (fechaInicio.equals("") != true) && (fechaFin.equals("") != true) &&(descripcion.equals("") !=true)) {
+          
+            promo.editarPromociones(promocion);
+            Promocion promocion2 = new Promocion(idPromocion,nombre,fechaInicio,fechaFin,montoDescuento, descripcion);
+            promocion2.setFecha_inicio(dpFechaInicio.getValue());
+            promocion2.setFecha_fin(dpFechaFin.getValue());
+            int posicionSeleccionado = tablaPromociones.getSelectionModel().getSelectedIndex();
+            promocionest.set(posicionSeleccionado, promocion2);
+            limpiar();
+        } else {
+            JOptionPane.showMessageDialog(null, "Rellene todos los campos");
+        }
+        btnEliminar.setVisible(true);
+        btnModificar.setVisible(true);
+        btnCancelar.setVisible(false);
+        btnGuardar.setVisible(false);
+    }
+     @FXML
+    private void limpiar() {
+        tfnombre.setText("");
+        tfDescuento.setText("");
+        dpFechaInicio.getEditor().clear();
+        dpFechaFin.getEditor().clear();
+        taDescripcion.setText("");
+        tfID.setText("");
+    }
+    @FXML
+    private void cancelarAccion(ActionEvent event){
+       tfnombre.setText("");
+        tfDescuento.setText("");
+        taDescripcion.setText("");
+        btnEliminar.setVisible(true);
+        btnModificar.setVisible(true);
+        btnCancelar.setVisible(false);
+        btnGuardar.setVisible(false);
+        dpFechaInicio.getEditor().clear();
+        dpFechaFin.getEditor().clear();
+       
+    }
+    
+    @FXML
+    private void eliminarPromocion(ActionEvent event) {
+        Promocion promocion = obtenerPromocionSeleccionada();
+        int posicionSeleccionado = tablaPromociones.getSelectionModel().getSelectedIndex();
+        if (posicionSeleccionado != -1) {
+            int respuesta = JOptionPane.showConfirmDialog(null,"¿Desea eliminar la promocion?", "Eliminar promocion",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if(respuesta == 0){
+              promo.eliminarPromocion(promocion);
+              promocionest.remove(posicionSeleccionado);
+              }
+         } else {
+            JOptionPane.showMessageDialog(null, "Debe de selecionar primero una promocion de la tabla");
+        }
+    }
+ /*   private void ventanaAgregarPromocion(ActionEvent e) throws IOException{
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/agregarPromocion.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        Stage stage = new Stage();
+        stage.setTitle("Registro de Promociones");
+        stage.setScene(scene);
+        stage.show(); 
+    }*/
+ /*   public void mostrarVentanaRegistro(ActionEvent event) throws Exception {    
+     try { 
+       System.out.println("no lo obtine");
+       Parent root = FXMLLoader.load(getClass().getResource("/fxml/agregarPromocion.fxml")); 
+       System.out.println("problema al cargarlo");
+        
+       System.out.println("no llega al stage");
+       Stage stage = new Stage(); 
+       System.out.println("aqui prmero");
+       stage.setScene(new Scene(root)); 
+       System.out.println("aqui paso alfo");
+       stage.show(); 
+     } catch(Exception e) { 
+      e.printStackTrace(); 
+      } 
+} */
+    public void setProgramaPrincipal(MainApp programaPrincipal) {
+        this.programaPrincipal = programaPrincipal; 
+       
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cargarTablaPromociones();
+        tfID.setDisable(true);     
+    }    
+    
+   /* @FXML
+    public void ventanaRegistro(ActionEvent event){
+        try {
+          System.out.println("si");
+            programaPrincipal.mostrarVentanaRegistroPromocion();
+            System.out.println("no");
+        } catch (IOException ex) {
+          ex.printStackTrace();
+            Logger.getLogger(FXMLAgregarPromocionController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+    }*/
+}
